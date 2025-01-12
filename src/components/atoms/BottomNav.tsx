acaccf66ce1c443f919b9d5d37bccc53f4cd72b7
Kerim <MCKerim@gmx.de>
2025-01-12 00:31:21 +0000

    Better Bottom nav

--- src/components/atoms/BottomNav.tsx
+++ src/components/atoms/BottomNav.tsx
@@ -4,58 +4,61 @@ import { Separator } from "../ui/separator";
 
 export default function BottomNav() {
   return (
-    <div className="w-full max-w-lg fixed bottom-0 mb-2">
-      <Separator className="mt-4 mb-2" />
+    <>
+      <div style={{ height: "100px" }}></div>
+      <div className="w-full max-w-lg fixed bottom-0 pb-2 bg-background">
+        <Separator className="mb-2" />
 
-      <div className="flex justify-between w-full gap-1 px-2">
-        <NavLink to="/" className="w-full">
-          {({ isActive }) => (
-            <Button
-              className={
-                (isActive ? "bg-gray-700 hover:bg-gray-700" : "") + " w-full"
-              }
-            >
-              1
-            </Button>
-          )}
-        </NavLink>
+        <div className="flex justify-between w-full gap-1 px-2">
+          <NavLink to="/" className="w-full">
+            {({ isActive }) => (
+              <Button
+                className={
+                  (isActive ? "bg-gray-700 hover:bg-gray-700" : "") + " w-full"
+                }
+              >
+                1
+              </Button>
+            )}
+          </NavLink>
 
-        <NavLink to="/mealplanner" className="w-full">
-          {({ isActive }) => (
-            <Button
-              className={
-                (isActive ? "bg-gray-700 hover:bg-gray-700" : "") + " w-full"
-              }
-            >
-              2
-            </Button>
-          )}
-        </NavLink>
+          <NavLink to="/mealplanner" className="w-full">
+            {({ isActive }) => (
+              <Button
+                className={
+                  (isActive ? "bg-gray-700 hover:bg-gray-700" : "") + " w-full"
+                }
+              >
+                2
+              </Button>
+            )}
+          </NavLink>
 
-        <NavLink to="/discover" className="w-full">
-          {({ isActive }) => (
-            <Button
-              className={
-                (isActive ? "bg-gray-700 hover:bg-gray-700" : "") + " w-full"
-              }
-            >
-              3
-            </Button>
-          )}
-        </NavLink>
+          <NavLink to="/discover" className="w-full">
+            {({ isActive }) => (
+              <Button
+                className={
+                  (isActive ? "bg-gray-700 hover:bg-gray-700" : "") + " w-full"
+                }
+              >
+                3
+              </Button>
+            )}
+          </NavLink>
 
-        <NavLink to="/bookmarks" className="w-full">
-          {({ isActive }) => (
-            <Button
-              className={
-                (isActive ? "bg-gray-700 hover:bg-gray-700" : "") + " w-full"
-              }
-            >
-              4
-            </Button>
-          )}
-        </NavLink>
+          <NavLink to="/bookmarks" className="w-full">
+            {({ isActive }) => (
+              <Button
+                className={
+                  (isActive ? "bg-gray-700 hover:bg-gray-700" : "") + " w-full"
+                }
+              >
+                4
+              </Button>
+            )}
+          </NavLink>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
