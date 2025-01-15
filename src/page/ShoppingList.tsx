84f6b0595ceddf0386e14d48b0b23098e8c1f7f9
Kerim <MCKerim@gmx.de>
2025-01-15 21:05:35 +0000

    Added Amount and name input

--- src/page/ShoppingList.tsx
+++ src/page/ShoppingList.tsx
@@ -3,6 +3,8 @@ import ShoppingItem from "@/components/atoms/ShoppingItem";
 import { useEffect, useState } from "react";
 import supabase from "@/utils/supabase";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 type Item = {
   name: string;
@@ -12,6 +14,8 @@ type Item = {
 
 export default function ShoppingList() {
   const [items, setItems] = useState<Item[]>([]);
+  const [newItemName, setNewItemName] = useState("");
+  const [newItemAmount, setNewItemAmount] = useState("");
 
   useEffect(() => {
     getItems();
@@ -44,9 +48,31 @@ export default function ShoppingList() {
   }
 
   async function addItem() {
+    if (!newItemName || !newItemAmount) {
+      return;
+    }
+    const { data } = await supabase
+      .from("item")
+      .insert([{ name: newItemName }])
+      .select();
+
+    setNewItemName("");
+
+    if (data) {
+      addToShoppingList(data[0].id);
+    }
+  }
+
+  async function addToShoppingList(itemId: number) {
+    const amount = parseInt(newItemAmount);
+    if(isNaN(amount)) return;
+    setNewItemAmount("");
+
     const { data } = await supabase
       .from("shopping_list_items")
-      .insert([{ shopping_list_id: 1, item_id: 1, amount: 30, bought: true }])
+      .insert([
+        { shopping_list_id: 1, item_id: itemId, amount: amount, bought: true },
+      ])
       .select();
 
     if (data) {
@@ -72,7 +98,30 @@ export default function ShoppingList() {
           onClick={() => handleItemClick(index)}
         />
       ))}
-      <div className="flex justify-end">
+
+      <div className="flex flex-col space-y-2 mt-4">
+        <div className="grid w-full items-center gap-1.5">
+          <Label htmlFor="itemInput">Item name</Label>
+          <Input
+            type="text"
+            id="itemInput"
+            placeholder="Appel"
+            value={newItemName}
+            onChange={(e) => setNewItemName(e.target.value)}
+          />
+        </div>
+
+        <div className="grid w-full items-center gap-1.5">
+          <Label htmlFor="amountInput">Amount</Label>
+          <Input
+            type="number"
+            id="amountInput"
+            placeholder="5"
+            value={newItemAmount}
+            onChange={(e) => setNewItemAmount(e.target.value)}
+          />
+        </div>
+
         <Button className="w-full" onClick={addItem}>
           Add Item
         </Button>
