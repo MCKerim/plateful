4db793a25bfd883acb21ee81694f5926e6443632
Kerim <MCKerim@gmx.de>
2025-01-17 21:07:01 +0000

    Get recipe data from supabase

--- src/page/Recipe.tsx
+++ src/page/Recipe.tsx
@@ -1,14 +1,47 @@
 import Layout from "@/components/layout/Layout";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { Button } from "@/components/ui/button";
+import supabase from "@/utils/supabase";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+
+type Recipe = {
+  id: number;
+  recipeName: string;
+  description: string;
+};
 
 export default function Recipe() {
   const params = useParams();
 
+  const [recipe, setRecipe] = useState<Recipe | null>(null);
+
+  useEffect(() => {
+    getRecipe();
+  }, [params.recipeId]);
+
+  async function getRecipe() {
+    if (!params.recipeId) return;
+    const { data } = await supabase
+      .from("recipes")
+      .select()
+      .eq("id", params.recipeId);
+
+    if (!data) {
+      setRecipe(null);
+      return;
+    }
+
+    setRecipe({
+      id: data[0].id,
+      recipeName: data[0].name,
+      description: data[0].description,
+    });
+  }
+
   return (
     <Layout>
-      <h1 className="text-2xl font-bold">Spaghetti Carbonara {params.recipeId}</h1>
+      <h1 className="text-2xl font-bold">{recipe?.recipeName}</h1>
 
       <AspectRatio ratio={16 / 9} className="bg-muted">
         <img
@@ -18,7 +51,7 @@ export default function Recipe() {
         />
       </AspectRatio>
 
-      <p className="font-bold">A classic Italian pasta dish</p>
+      <p className="font-bold">{recipe?.description}</p>
 
       <p>
         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
@@ -28,9 +61,9 @@ export default function Recipe() {
 
       <div className="flex gap-2 w-full mt-11">
         <Button className="w-full" variant="secondary">
-          Plan
+          Save
         </Button>
-        <Button className="w-full">Save</Button>
+        <Button className="w-full">Plan</Button>
       </div>
     </Layout>
   );
