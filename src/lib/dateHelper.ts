a6f012ea59e0a8c7e9934b6cbc0ece5201c786b7
Kerim <MCKerim@gmx.de>
2025-03-21 19:48:33 +0000

    better date helper

--- src/lib/dateHelper.ts
+++ src/lib/dateHelper.ts
@@ -12,24 +12,27 @@ export function toWeekday(date: Date): string {
   const today = new Date();
   const yesterday = new Date(today);
   yesterday.setDate(today.getDate() - 1);
-  const dayBeforeYesterday = new Date(today);
-  dayBeforeYesterday.setDate(today.getDate() - 2);
   const tomorrow = new Date(today);
   tomorrow.setDate(today.getDate() + 1);
 
+  let prefix = "";
+  let dayAsWord = "";
+
   if (today.toDateString() === date.toDateString()) {
-    return "Today";
+    dayAsWord = "Today";
   } else if (yesterday.toDateString() === date.toDateString()) {
-    return "Yesterday";
-  } else if (dayBeforeYesterday.toDateString() === date.toDateString()) {
-    return "the day before yesterday";
+    dayAsWord = "Yesterday";
   } else if (tomorrow.toDateString() === date.toDateString()) {
-    return "Tomorrow";
+    dayAsWord = "Tomorrow";
   } else {
-    let prefix = "";
     if (today > date) {
       prefix = "last ";
     }
-    return prefix + weekday[date.getDay()];
+    dayAsWord = weekday[date.getDay()];
   }
+  dayAsWord += " • ";
+
+  const formattedDate = `${String(date.getDate()).padStart(2, '0')}.${String(date.getMonth() + 1).padStart(2, '0')}.${date.getFullYear()}`;
+
+  return prefix + dayAsWord + formattedDate;
 }
