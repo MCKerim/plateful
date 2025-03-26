38ee05770edeb8bb1da432d1d5c1ae556a4b4ff5
Kerim <MCKerim@gmx.de>
2025-03-26 14:39:31 +0000

    implemeted fuzzy recipe search with fuse js

--- src/page/Discover.tsx
+++ src/page/Discover.tsx
@@ -1,9 +1,11 @@
 import RecipeCard from "@/components/atoms/RecipeCard";
 import Layout from "@/components/layout/Layout";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import supabase from "@/utils/supabase";
 import { useEffect, useState } from "react";
 import { Link } from "react-router";
+import Fuse from "fuse.js";
 
 type Recipe = {
   id: number;
@@ -13,10 +15,16 @@ type Recipe = {
 
 export default function Discover() {
   const [recipes, setRecipes] = useState<Recipe[]>([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState<Recipe[]>([]);
 
   useEffect(() => {
     getRecipes();
   }, []);
+
+  useEffect(() => {
+    handleSearch();
+  }, [searchTerm]);
 
   async function getRecipes() {
     const { data } = await supabase
@@ -34,6 +42,7 @@ export default function Discover() {
 
     if (!data) {
       setRecipes(newRecipes);
+      setSearchResults(newRecipes);
       return;
     }
 
@@ -47,14 +56,38 @@ export default function Discover() {
     });
     console.log("Items: ", newRecipes);
     setRecipes(newRecipes);
+    setSearchResults(newRecipes);
   }
+
+  const fuse = new Fuse(recipes, {
+    keys: ["recipeName"],
+    includeScore: true, // Provides a score for how good the match is
+    threshold: 0.4, // Lower threshold = more strict matching
+    distance: 100, // Maximum distance for a match
+  });
+
+  const handleSearch = async () => {
+    console.log("searc");
+    if (searchTerm.trim() === "") {
+      setSearchResults(recipes);
+      return;
+    }
+
+    const results = fuse.search(searchTerm);
+    const matchedRecipes = results.map((result) => result.item);
+
+    setSearchResults(matchedRecipes);
+  };
 
   return (
     <Layout>
-      <h1 className="text-2xl">Discover</h1>
+      <h1 className="text-2xl">
+        Discover{" "}
+        {searchResults.length > 0 && `• ${searchResults.length} recipes`}
+      </h1>
 
       <div className="flex flex-col gap-2 items-center">
-        {recipes.map((recipe, index) => (
+        {searchResults.map((recipe, index) => (
           <RecipeCard
             key={index}
             id={recipe.id}
@@ -63,10 +96,21 @@ export default function Discover() {
           />
         ))}
       </div>
-      <div className="fixed bottom-20">
-        <Button asChild>
-          <Link to="/recipe/add">Add Recipe</Link>
-        </Button>
+
+      <div className="w-full max-w-lg pr-8 fixed bottom-20 p-0 m-0 bg-background">
+        <div className="flex w-full items-center my-2 gap-4">
+          <Input
+            className="w-full"
+            type="text"
+            placeholder="Enter recipe name..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+
+          <Button asChild>
+            <Link to="/recipe/add">Add Recipe</Link>
+          </Button>
+        </div>
       </div>
     </Layout>
   );
