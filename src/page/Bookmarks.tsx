c7be0038734737b6692c65af611751b214784aaf
Kerim <MCKerim@gmx.de>
2025-03-27 11:58:18 +0000

    Added translations for settings

--- src/page/Bookmarks.tsx
+++ src/page/Bookmarks.tsx
@@ -2,11 +2,11 @@ import Layout from "@/components/layout/Layout";
 import { useTranslation } from "react-i18next";
 
 export default function Bookmarks() {
-  const { i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
 
   return (
     <Layout>
-      <h1 className="text-2xl">Einstellungen</h1>
+      <h1 className="text-2xl">{t("settings.title")}</h1>
       
       <button onClick={() => i18n.changeLanguage('en')}>English</button>
       
