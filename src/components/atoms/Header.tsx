921e1a1726fd9e8a13cecc9ab0a45abb4e2e719e
MCKerim <MCKerim@gmx.de>
2025-03-30 13:11:55 +0200

    moved sign out button to settings

--- src/components/atoms/Header.tsx
+++ src/components/atoms/Header.tsx
@@ -1,16 +1,7 @@
-import supabase from "@/utils/supabase";
 import { ModeToggle } from "../mode-toggle";
 import { Separator } from "../ui/separator";
-import { Button } from "../ui/button";
 
 export default function Header() {
-  const signOut = async () => {
-    const { error } = await supabase.auth.signOut();
-    if (error) {
-      console.error("Error while sign out: ", error);
-    }
-  };
-
   return (
     <>
       <div style={{ height: "64px" }}></div>
@@ -18,9 +9,7 @@ export default function Header() {
       <div className="w-full max-w-lg fixed top-0 pt-2 bg-background">
         <div className="flex justify-between w-full items-center px-2">
           <h1 className="font-bold text-2xl">Plateful</h1>
-          <Button variant="outline" onClick={signOut}>
-            Sign out
-          </Button>
+
           <ModeToggle />
         </div>
 
--- src/page/Bookmarks.tsx
+++ src/page/Bookmarks.tsx
@@ -1,21 +1,45 @@
 import Layout from "@/components/layout/Layout";
 import { Button } from "@/components/ui/button";
+import supabase from "@/utils/supabase";
 import { useTranslation } from "react-i18next";
 
 export default function Bookmarks() {
   const { t, i18n } = useTranslation();
 
+  const signOut = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      console.error("Error while sign out: ", error);
+    }
+  };
+
   return (
     <Layout>
       <h1 className="text-2xl">{t("settings.title")}</h1>
-      
+
       <h2>{t("settings.language")}</h2>
 
       <div className="w-full flex gap-2">
-        <Button className="w-full" variant="secondary" onClick={() => i18n.changeLanguage('en')}>English</Button>
-        
-        <Button className="w-full" variant="secondary" onClick={() => i18n.changeLanguage('de')}>Deutsch</Button>
+        <Button
+          className="w-full"
+          variant="secondary"
+          onClick={() => i18n.changeLanguage("en")}
+        >
+          English
+        </Button>
+
+        <Button
+          className="w-full"
+          variant="secondary"
+          onClick={() => i18n.changeLanguage("de")}
+        >
+          Deutsch
+        </Button>
       </div>
+
+      <Button variant="outline" onClick={signOut}>
+        Sign out
+      </Button>
     </Layout>
   );
 }
