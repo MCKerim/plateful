741280dfc5cbc74f3d515fe8fba6e129f1130213
Kerim <MCKerim@gmx.de>
2025-04-02 20:00:43 +0000

    made comming soon overlay right color

--- src/components/ui/commingSoonOverlay.tsx
+++ src/components/ui/commingSoonOverlay.tsx
@@ -5,9 +5,11 @@ export function CommingSoon() {
   const { t } = useTranslation();
 
   return (
-    <div className="bg-black absolute left-0 top-0 opacity-90 w-full h-full z-10 items-center flex flex-col justify-center">
+    <div className="bg-secondary opacity-70 text-secondary-foreground absolute left-0 top-0 w-full h-full z-10 items-center flex flex-col justify-center">
       <div className="flex gap-2 text-2xl items-center justify-center font-bold">
-        <h1 className="italic">{t("common.commingSoon")}</h1>
+        <h1 className="italic">
+          {t("common.commingSoon")}
+        </h1>
 
         <AlarmClock />
       </div>
