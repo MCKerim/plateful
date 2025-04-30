aa282c50fb50ace287b012349a6746d6fffc62a4
Kerim <MCKerim@gmx.de>
2025-04-30 20:02:02 +0000

    made mode toggle three buttons

--- src/components/atoms/mode-toggle.tsx
+++ src/components/atoms/mode-toggle.tsx
@@ -1,45 +1,39 @@
 import { Moon, Sun } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { useTheme } from "@/components/atoms/theme-provider";
 import { useTranslation } from "react-i18next";
 
 export function ModeToggle() {
   const { t } = useTranslation();
 
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+    <div className="flex w-full gap-2">
+      <Button
+        variant={theme === "light" ? "default" : "secondary"}
+        className="w-full"
+        onClick={() => setTheme("light")}
+      >
+        <Sun /> {t("settings.modeToggle.light")}
+      </Button>
 
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <Button
+        variant={theme === "dark" ? "default" : "secondary"}
+        className="w-full"
+        onClick={() => setTheme("dark")}
+      >
+        <Moon /> {t("settings.modeToggle.dark")}
+      </Button>
 
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          {t("settings.modeToggle.light")}
-        </DropdownMenuItem>
-
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          {t("settings.modeToggle.dark")}
-        </DropdownMenuItem>
-
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          {t("settings.modeToggle.system")}
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+      <Button
+        variant={theme === "system" ? "default" : "secondary"}
+        className="w-full"
+        onClick={() => setTheme("system")}
+      >
+        {t("settings.modeToggle.system")}
+      </Button>
+    </div>
   );
 }
