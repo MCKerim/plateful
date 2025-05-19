40e41b5be46cfde6980c330f1b6090560e0dca10
Kerim <MCKerim@gmx.de>
2025-05-19 20:08:49 +0000

    refactoring

--- src/store.ts
+++ src/store.ts
@@ -1,22 +1,21 @@
-import type { Action, ThunkAction } from '@reduxjs/toolkit'
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from './userSlice'
+import type { Action, ThunkAction } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./userSlice";
 
 export const store = configureStore({
   reducer: {
-    user: userReducer
-  }
-})
+    user: userReducer,
+  },
+});
 
-// Infer the type of `store`
-export type AppStore = typeof store
-export type RootState = ReturnType<AppStore['getState']>
-// Infer the `AppDispatch` type from the store itself
-export type AppDispatch = AppStore['dispatch']
-// Define a reusable type describing thunk functions
+export type AppStore = typeof store;
+export type RootState = ReturnType<AppStore["getState"]>;
+
+export type AppDispatch = AppStore["dispatch"];
+
 export type AppThunk<ThunkReturnType = void> = ThunkAction<
   ThunkReturnType,
   RootState,
   unknown,
   Action
->
+>;
--- src/userSlice.ts
+++ src/userSlice.ts
@@ -1,47 +1,34 @@
-import type { PayloadAction } from "@reduxjs/toolkit"
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
-import { RootState } from "./store"
+import type { PayloadAction } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "./store";
 
 export interface UserState {
-  value: number
-  status: 'idle' | 'loading' | 'failed'
+  value: number;
 }
 
 const initialState: UserState = {
   value: 0,
-  status: 'idle'
-}
+};
 
 export const userSlice = createSlice({
-  name: 'user',
+  name: "user",
   initialState,
 
   reducers: {
-    increment: state => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1
+    increment: (state) => {
+      state.value += 1;
     },
-    decrement: state => {
-      state.value -= 1
+    decrement: (state) => {
+      state.value -= 1;
     },
-    // Use the PayloadAction type to declare the contents of `action.payload`
     incrementByAmount: (state, action: PayloadAction<number>) => {
-      state.value += action.payload
-    }
-  }
-})
+      state.value += action.payload;
+    },
+  },
+});
 
-// Export the generated action creators for use in components
-export const { increment, decrement, incrementByAmount } = userSlice.actions
+export const { increment, decrement, incrementByAmount } = userSlice.actions;
 
-// Export the slice reducer for use in the store configuration
-export default userSlice.reducer
+export default userSlice.reducer;
 
-// Selector functions allows us to select a value from the Redux root state.
-// Selectors can also be defined inline in the `useSelector` call
-// in a component, or inside the `createSlice.selectors` field.
-export const selectCount = (state: RootState) => state.user.value
-export const selectStatus = (state: RootState) => state.user.status
+export const selectCount = (state: RootState) => state.user.value;
