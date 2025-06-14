4d440ce74d7f9023a98b2499c0ae413bd94d2ef0
Kerim <MCKerim@gmx.de>
2025-06-14 14:16:42 +0200

    fix muted color

--- src/components/atoms/BottomNav.tsx
+++ src/components/atoms/BottomNav.tsx
@@ -8,7 +8,7 @@ export default function BottomNav() {
     <>
       <div style={{ height: "100px" }}></div>
 
-      <div className="w-full max-w-lg fixed bottom-0 py-2 bg-secondary z-20 rounded-t-2xl">
+      <div className="w-full max-w-lg fixed bottom-0 py-3 bg-secondary z-20 rounded-t-2xl">
         <div className="flex justify-between w-full gap-1 px-2">
           <BottomNavButton
             label={t("bottomNav.home")}
