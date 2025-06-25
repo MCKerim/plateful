1175f386a7c717ef824c56f47757ec1e71281ffb
Kerim <MCKerim@gmx.de>
2025-06-25 20:26:08 +0200

    Better Signup page

--- src/page/onboarding/signUp/SignUp.tsx
+++ src/page/onboarding/signUp/SignUp.tsx
@@ -38,8 +38,8 @@ export default function SignUp() {
     <div className="flex flex-col items-center justify-between h-screen px-4 py-10">
       <div></div>
       <div className="text-center mb-8">
-        <h1 className="text-4xl font-bold mb-4">
-          Welcome to <span className="text-plateful">Plateful!</span>
+        <h1 className="text-5xl font-bold mb-4">
+          Plateful
         </h1>
 
         <p className="text-sm text-muted-foreground italic">
@@ -48,7 +48,7 @@ export default function SignUp() {
       </div>
 
       <div className="w-full max-w-sm flex flex-col gap-4">
-        <Button className="w-full rounded-full" onClick={signUp}>
+        <Button className="w-full h-12 font-semibold text-base shadow-lg shadow-primary/20 rounded-full" onClick={signUp}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
             <path
               d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
@@ -56,7 +56,7 @@ export default function SignUp() {
             />
           </svg>
 
-          Login with Google
+          Continue with Google
         </Button>
 
         <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
