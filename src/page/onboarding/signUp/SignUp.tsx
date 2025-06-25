e968eefe007999bee26a32fdea9529721db6dc65
Kerim <MCKerim@gmx.de>
2025-06-25 20:16:24 +0200

    bette rwelcome page

--- src/page/onboarding/signUp/SignUp.tsx
+++ src/page/onboarding/signUp/SignUp.tsx
@@ -1,11 +1,4 @@
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import supabase from "@/utils/supabase";
 
 export default function SignUp() {
@@ -42,36 +35,35 @@ export default function SignUp() {
   };
 
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm flex flex-col gap-6">
-        <Card>
-          <CardHeader className="text-center">
-            <CardTitle className="text-xl">
-              Welcome to Plateful!
-            </CardTitle>
-          </CardHeader>
+    <div className="flex flex-col items-center justify-between h-screen px-4 py-10">
+      <div></div>
+      <div className="text-center mb-8">
+        <h1 className="text-4xl font-bold mb-4">
+          Welcome to <span className="text-plateful">Plateful!</span>
+        </h1>
 
-          <CardContent className="flex flex-col gap-2">
-            <Button variant="outline" className="w-full" onClick={signUp}>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <path
-                  d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                  fill="currentColor"
-                />
-              </svg>
+        <p className="text-sm text-muted-foreground italic">
+          The Meal Planner for casual people
+        </p>
+      </div>
 
-              Login with Google
-            </Button>
-          </CardContent>
+      <div className="w-full max-w-sm flex flex-col gap-4">
+        <Button className="w-full rounded-full" onClick={signUp}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path
+              d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+              fill="currentColor"
+            />
+          </svg>
 
-          <CardFooter>
-            <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
-              By continuing, you agree to our{" "}
-              <a href="#">Terms of Service</a> and{" "}
-              <a href="#">Privacy Policy</a>.
-            </div>
-          </CardFooter>
-        </Card>
+          Login with Google
+        </Button>
+
+        <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
+          By continuing, you agree to our 
+          {" "}<a href="#">Terms of Service</a> and{" "}
+          <a href="#">Privacy Policy</a>.
+        </div>
       </div>
     </div>
   );
