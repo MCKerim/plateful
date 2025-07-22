fd22a7cdd8bd3802f093ed324ce71960e0bff164
Kerim <MCKerim@gmx.de>
2025-07-22 22:44:57 +0200

    some translations

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -3,8 +3,10 @@ import { CommingSoon } from "@/components/ui/commingSoonOverlay";
 import { useAppSelector } from "@/redux/hooks";
 import { selectHousehold } from "@/redux/slices/householdSlice";
 import { House } from "lucide-react";
+import { useTranslation } from "react-i18next";
 
 export default function Home() {
+  const { t } = useTranslation();
   const household = useAppSelector(selectHousehold);
 
   return (
@@ -12,12 +14,14 @@ export default function Home() {
       <div className="flex items-center gap-2">
         <House />
 
-        <h1 className="text-2xl font-bold flex items-center gap-2">
+        <h1 className="flex items-center gap-2 text-2xl font-bold">
           {household?.name}
         </h1>
       </div>
 
-      <p className="mt-2 italic text-muted-foreground">Viel Spaß beim Kochen</p>
+      <p className="mt-2 italic text-muted-foreground">
+        {t("home.enjoyCooking")}
+      </p>
 
       <CommingSoon />
     </Layout>
