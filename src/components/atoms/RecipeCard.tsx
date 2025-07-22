54f32a06faa77b8b087a0a2e4d7500a26e0e46bf
Kerim <MCKerim@gmx.de>
2025-07-22 22:23:57 +0200

    darker placeholder img

--- src/components/atoms/RecipeCard.tsx
+++ src/components/atoms/RecipeCard.tsx
@@ -108,12 +108,9 @@ export default function RecipeCard({ id, name }: Readonly<Props>) {
     <NavLink to={`/recipe/${id}`} className="w-full">
       <Card className="relative">
         <img
-          src={
-            imageUrl ||
-            "https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
-          }
+          src={imageUrl || "/no-img.jpg"}
           alt="Recipe"
-          className="h-32 w-full object-cover border-b-4 border-background"
+          className="h-32 w-full object-cover border-b-4 border-background dark:brightness-75"
         />
 
         <div className="p-2">
@@ -124,7 +121,9 @@ export default function RecipeCard({ id, name }: Readonly<Props>) {
           </div>
 
           <div className="flex justify-between">
-            <h1 className="font-bold text-md leading-tight line-clamp-2">{name}</h1>
+            <h1 className="font-bold text-md leading-tight line-clamp-2">
+              {name}
+            </h1>
           </div>
 
           <div className="flex justify-between mt-2">
