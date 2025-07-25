0c96a09ae7570d9ea70984fa554271223cf2c2cd
Kerim <MCKerim@gmx.de>
2025-07-25 13:25:49 +0200

    set default theme to light

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
           <App />
         </ThemeProvider>
       </BrowserRouter>
