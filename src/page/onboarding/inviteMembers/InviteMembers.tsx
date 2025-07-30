98d1b591c4711a512cc2eec1613012d6f2b65cb9
Kerim <MCKerim@gmx.de>
2025-07-31 01:53:41 +0200

    translations

--- src/page/onboarding/inviteMembers/InviteMembers.tsx
+++ src/page/onboarding/inviteMembers/InviteMembers.tsx
@@ -1,26 +1,31 @@
 import OnboardingLayout from "@/components/layout/onboardingLayout/OnboardingLayout";
 import InviteLink from "@/components/ui/inviteLink/InviteLink";
 import { useNavigate } from "react-router";
+import { useTranslation } from "react-i18next";
 
 export default function InviteMembers() {
   const navigate = useNavigate();
+  const { t } = useTranslation();
 
   async function completeScreen() {
     navigate("/");
   }
 
   return (
-    <OnboardingLayout nextButtonLabel="Fertig!" onNext={completeScreen}>
+    <OnboardingLayout
+      nextButtonLabel={t("inviteMembers.nextButton")}
+      onNext={completeScreen}
+    >
       <div className="text-center">
-        <h1 className="text-3xl font-bold">Füge Mitglieder hinzu</h1>
-
-        <p className="text-gray-600 max-w-sm mt-2">
-          Lade Freunde oder Familie in dein Haushalt ein. Scanne dazu den
-          QR-Code oder teile den Link.
-        </p>
+        <h1 className="text-3xl font-bold">{t("inviteMembers.title")}</h1>
       </div>
 
-      <InviteLink />
+      <div>
+        <p className="max-w-sm mb-12 font-medium text-center text-primary">
+          {t("inviteMembers.description")}
+        </p>
+        <InviteLink />
+      </div>
     </OnboardingLayout>
   );
 }
