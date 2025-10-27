78f8361a9b157bdebbe2bef19d96836059a6e460
MCKerim <MCKerim@gmx.de>
2025-10-27 11:58:33 +0100

    space

--- src/main.tsx
+++ src/main.tsx
@@ -15,6 +15,7 @@ createRoot(document.getElementById("root")!).render(
       <SupabaseProvider>
         <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
           <AppUrlListener />
+
           <App />
         </ThemeProvider>
       </SupabaseProvider>
