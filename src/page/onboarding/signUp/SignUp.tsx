a07fa0c731df5104117fbfb4182641f8a41f06c2
Kerim <MCKerim@gmx.de>
2025-11-11 23:16:45 +0100

    added email

--- src/page/onboarding/signUp/SignUp.tsx
+++ src/page/onboarding/signUp/SignUp.tsx
@@ -5,10 +5,12 @@ import { useRive } from "@rive-app/react-canvas";
 import { useEffect, useState } from "react";
 import CircleTransition from "@/components/atoms/CircleTransition";
 import { openBrowser } from "@/utils/nativeBrowser";
+import { useNavigate } from "react-router";
 
 export default function SignUp() {
   const { supabase } = useSupabase();
   const { t } = useTranslation();
+  const navigate = useNavigate();
   const [showTransition, setShowTransition] = useState(true);
 
   const { rive, RiveComponent } = useRive({
@@ -108,6 +110,19 @@ export default function SignUp() {
           <OnboardingButton
             label={t("signup.continueWithGoogle")}
             onClick={signUp}
+            icon={
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path
+                  d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                  fill="currentColor"
+                />
+              </svg>
+            }
+          />
+
+          <OnboardingButton
+            label={t("signup.continueWithEMail")}
+            onClick={() => navigate("")}
             icon={
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                 <path
