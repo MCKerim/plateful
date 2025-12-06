08696984c3bcf6477d4e0bb4dfe26c292ab2070c
Kerim <MCKerim@gmx.de>
2025-12-06 12:19:28 +0100

    finished first version of add recipe drawer

--- src/components/atoms/AddRecipeDrawer.tsx
+++ src/components/atoms/AddRecipeDrawer.tsx
@@ -2,13 +2,10 @@ import { Button } from "../ui/button";
 import {
   Drawer,
   DrawerContent,
-  DrawerDescription,
   DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
   DrawerTrigger,
 } from "../ui/drawer";
-import { BadgePlus, Camera, Link, Plus } from "lucide-react";
+import { Camera, Link, Plus } from "lucide-react";
 
 type Props = {
   newRecipeClicked: () => void;
@@ -17,6 +14,29 @@ type Props = {
 export default function AddNewRecipeDrawer({
   newRecipeClicked,
 }: Readonly<Props>) {
+  function renderButton(
+    icon: any,
+    label: string,
+    onClick: () => void,
+    description?: string
+  ) {
+    return (
+      <Button variant="secondary" size="lg" onClick={onClick}>
+        <div className="flex justify-start gap-4 w-full h-full items-center text-start">
+          {icon}
+
+          <div className="flex flex-col justify-start">
+            <p>{label}</p>
+
+            <p className="text-xs font-normal text-muted-foreground">
+              {description}
+            </p>
+          </div>
+        </div>
+      </Button>
+    );
+  }
+
   return (
     <Drawer>
       <DrawerTrigger className="p-2.5 fixed bottom-[5rem] right-[1rem] z-10 rounded-full bg-accent text-accent-foreground flex items-center justify-center shadow-lg transition-all duration-300 hover:scale-110 hover:bg-plateful/90 active:scale-95">
@@ -24,29 +44,22 @@ export default function AddNewRecipeDrawer({
       </DrawerTrigger>
 
       <DrawerContent>
-        <DrawerHeader>
-          <DrawerTitle>Rezept hinzufügen</DrawerTitle>
+        <DrawerFooter className="gap-4 mb-8 mt-4">
+          {renderButton(
+            <Link />,
+            "Rezept aus URL hinzufügen",
+            () => {},
+            "TikTok, YouTube, Instagram, Webseite"
+          )}
 
-          <DrawerDescription>
-            Wie möchtest du das Rezept hinzufügen?
-          </DrawerDescription>
-        </DrawerHeader>
+          {renderButton(
+            <Camera />,
+            "Rezept aus Foto hinzufügen",
+            () => {},
+            "Scanne ein Rezeptbuch oder Gericht"
+          )}
 
-        <DrawerFooter className="gap-4 mb-10">
-          <Button variant="secondary" size="lg" disabled>
-            <Link />
-            aus URL hinzufügen
-          </Button>
-
-          <Button variant="secondary" size="lg" disabled>
-            <Camera />
-            aus Foto hinzufügen
-          </Button>
-
-          <Button variant="secondary" size="lg" onClick={newRecipeClicked}>
-            <BadgePlus />
-            Neues Rezept erstellen
-          </Button>
+          {renderButton(<Plus />, "Neues Rezept erstellen", newRecipeClicked)}
         </DrawerFooter>
       </DrawerContent>
     </Drawer>
