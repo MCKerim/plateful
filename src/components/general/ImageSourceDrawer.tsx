79f77ed26c7f97fcda19bcdf7b87de88d8cf4a31
MCKerim <MCKerim@gmx.de>
2026-01-23 14:30:00 +0100

    better drawe rbnuttons

--- src/components/general/ImageSourceDrawer.tsx
+++ src/components/general/ImageSourceDrawer.tsx
@@ -24,26 +24,19 @@ export default function ImageSourceDrawer({
     icon: React.ReactNode,
     label: string,
     onClick: () => void,
-    description?: string,
-    disabled?: boolean
+    disabled?: boolean,
   ) {
     return (
       <Button
         variant="secondary"
-        size="lg"
+        className="w-full h-26 rounded-2xl"
         onClick={onClick}
         disabled={disabled}
       >
-        <div className="flex justify-start gap-4 w-full h-full items-center text-start">
+        <div className="flex flex-col gap-1 items-center">
           {icon}
 
-          <div className="flex flex-col justify-start">
-            <p className="second-font font-semibold">{label}</p>
-
-            <p className="text-xs font-normal text-muted-foreground">
-              {description}
-            </p>
-          </div>
+          <p className="second-font font-medium text-lg">{label}</p>
         </div>
       </Button>
     );
@@ -52,23 +45,21 @@ export default function ImageSourceDrawer({
   return (
     <Drawer open={open} onOpenChange={onOpenChange}>
       <DrawerContent>
-        <DrawerFooter className="gap-4 mb-8 mt-4">
-          {renderButton(
-            <Camera />,
-            t("common.imageSourceDrawer.camera.label"),
-            onCameraSelect,
-            isNative
-              ? t("common.imageSourceDrawer.camera.description")
-              : t("common.imageSourceDrawer.camera.disabledDescription"),
-            !isNative
-          )}
+        <DrawerFooter className="mb-8 mt-4">
+          <div className="flex gap-4">
+            {renderButton(
+              <Camera className="!size-8" />,
+              t("common.camera"),
+              onCameraSelect,
+              !isNative,
+            )}
 
-          {renderButton(
-            <ImageIcon />,
-            t("common.imageSourceDrawer.gallery.label"),
-            onGallerySelect,
-            t("common.imageSourceDrawer.gallery.description")
-          )}
+            {renderButton(
+              <ImageIcon className="!size-8" />,
+              t("common.gallery"),
+              onGallerySelect,
+            )}
+          </div>
         </DrawerFooter>
       </DrawerContent>
     </Drawer>
