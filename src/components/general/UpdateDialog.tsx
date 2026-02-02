b9cfe832303e9fc1a3377bd64042b8e98568b261
Kerim <MCKerim@gmx.de>
2026-02-02 10:18:12 +0100

    better dialog

--- src/components/general/UpdateDialog.tsx
+++ src/components/general/UpdateDialog.tsx
@@ -1,12 +1,8 @@
 import { Button } from "../ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-} from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { DialogDescription } from "@radix-ui/react-dialog";
 import { useTranslation } from "react-i18next";
+import Rive from "@rive-app/react-canvas";
 
 type Props = {
   open: boolean;
@@ -21,21 +17,19 @@ export default function UpdateDialog({ open, onConfirm, onCancel }: Readonly<Pro
     <Dialog open={open} onOpenChange={(isOpen) => !isOpen && onCancel()}>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
-          <DialogTitle>{t("update.title")}</DialogTitle>
+          <DialogTitle className="text-center first-font text-xl">{t("update.title")}</DialogTitle>
+
+          <div className="h-[150px] w-full">
+            <Rive src="/plateful-character.riv" artboard="Fly-In" />
+          </div>
         </DialogHeader>
 
         <DialogDescription className="flex flex-col gap-4 py-4">
-          <p>{t("update.updateAvailable")}</p>
+          <p className="text-center second-font">{t("update.updateAvailable")}</p>
 
-          <div className="flex gap-2">
-            <Button className="w-full" variant="secondary" onClick={onCancel}>
-              {t("common.cancel")}
-            </Button>
-
-            <Button className="w-full" onClick={onConfirm}>
-              {t("common.update")}
-            </Button>
-          </div>
+          <Button className="w-full" onClick={onConfirm}>
+            {t("update.updateNow")}
+          </Button>
         </DialogDescription>
       </DialogContent>
     </Dialog>
