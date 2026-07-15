e6f5d538e971cf3f3e8e54f7c46c908eb466c212
Kerim <MCKerim@gmx.de>
2026-07-15 21:40:57 +0200

    Android: bump version to 0.0.31

--- src/page/Settings.tsx
+++ src/page/Settings.tsx
@@ -237,7 +237,7 @@ export default function Settings() {
         <div className="flex flex-col gap-2 p-2 border rounded-lg">
           <h2 className="font-medium border-b">Info</h2>
 
-          <p className="text-sm">v0.0.30</p>
+          <p className="text-sm">v0.0.31</p>
 
           <div className="flex gap-2">
             <NavLink to="/privacy" className="w-full">
